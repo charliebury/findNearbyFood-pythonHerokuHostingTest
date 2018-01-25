9b972903679dc626aa3e0cc08f698ca164d7c3b3
Charlie Bury <csbury@me.com>
2018-01-25 00:54:42 +0000

    added option to log in as a user (saved to database)

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, jsonify, session, abort, g, json
 from findARestaurant import findARestaurant
-from models import Base, previousSearches
-
+from models import Base, User, previousSearches
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from flask_httpauth import HTTPBasicAuth
 import string
 import random
 
+auth = HTTPBasicAuth()
 
 engine = create_engine('sqlite:///findFoodUsers.db')
-
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 dbsession = DBSession()
@@ -17,6 +17,46 @@ dbsession = DBSession()
 app = Flask(__name__)
 
 app.secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+
+
+# verify whether a username/password or token are valid
+@auth.verify_password
+def verify_password(username_or_token, password):
+    user_id = User.verify_auth_token(username_or_token)
+    if user_id:
+        user = dbsession.query(User).filter_by(id=user_id).one()
+    else:
+        user = dbsession.query(User).filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+
+# get a new token (requires username/password to use)
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
+
+
+# make a new user and save to database for later use
+@app.route('/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        # not enough info supplied
+        abort(400)
+    if dbsession.query(User).filter_by(username=username).first() is not None:
+        # existing user
+        abort(400)
+    user = User(username=username)
+    user.hash_password(password)
+    dbsession.add(user)
+    dbsession.commit()
+    return jsonify({'username': username}), 201
 
 
 # homepage set to index.html
@@ -31,7 +71,7 @@ def goHome():
     return render_template('index.html')
 
 
-# find a restaurant based off what and where inputs
+# find a restaurant based off what/where inputs
 @app.route('/enterFoodWants', methods=['POST'])
 def enterFoodWants():
     if request.method == 'POST':
@@ -48,14 +88,17 @@ def enterFoodWants():
             return render_template('sorry.html')
 
 
-@app.route('/getFoodResult', methods=['GET', 'POST'])
+# supply json format information on restaurants to result.html
+@app.route('/getFoodResult', methods=['GET'])
 def getFoodResult():
     restaurantInfo = session.get('restaurantInfo')
     if restaurantInfo != "No Restaurants Found":
         return json.dumps(restaurantInfo)
 
 
+# logged in users can see all previous searches
 @app.route('/showPreviousSearches', methods=['GET'])
+@auth.login_required
 def showPreviousSearches():
     if request.method == 'GET':
         prevSearches = dbsession.query(previousSearches).all()
--- models.py
+++ models.py
@@ -3,11 +3,52 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
+import random
+import string
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
 
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+
+
+class User(Base):
+    # a database of saved users
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
+
+    # hash a supplied password
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    # check if supplied password matches hashed one
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    # create a token to avoid using password (will expire after some time)
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            # valid token, but expired
+            return None
+        except BadSignature:
+            # invalid token
+            return None
+        user_id = data['id']
+        return user_id
+
 
 class previousSearches(Base):
+    # a database of previous searches (by anyone)
     __tablename__ = 'bagel'
     id = Column(Integer, primary_key=True)
     location = Column(String)
@@ -15,7 +56,6 @@ class previousSearches(Base):
 
     @property
     def serialize(self):
-        """Return object data in easily serializeable format"""
         return {'location': self.location, 'foodType': self.foodType}
 
 engine = create_engine('sqlite:///findFoodUsers.db')
